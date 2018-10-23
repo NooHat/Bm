e72b46b0743c96a6abbb9937fca15f0eef79e275
NooHat <18156106065@163.com>
2018-10-23 14:59:18 +0800

    new commit 6.27

--- src/main/java/com/noh/Bm/indexCont.java
+++ src/main/java/com/noh/Bm/indexCont.java
@@ -5,16 +5,30 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 @RequestMapping("/index")
 public class indexCont {
-
+    final  String  viewroot ="html/Learn/";
     @ResponseBody
     @RequestMapping("/index")
     public String index(int i){
         int j=1/i;
 
         return "hello,Springboot";
+
+    }
+    @RequestMapping("/hellothymeleaf")
+    public String HelloThymeleaf(HttpServletRequest request){
+        System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaaaa");
+        System.out.println("bbbbbbbbbbbbbbbbbbbbbbbbbbb");
+        System.out.println("ccccccccccccccccccccccccccc");
+        request.setAttribute("helloMsg", "Hello,this is Thymeleaf!");
+
+        return viewroot+"helloThy";
+
+
 
     }
 
